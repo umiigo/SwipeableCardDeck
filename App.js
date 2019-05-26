0f339d3c420129042bd921158cad28a5a64b17c9
Umair <dev.umigo@gmail.com>
2019-05-26 23:58:00 +0100

    add profile photo from facebook

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
+
+const fbImage = "https://graph.facebook.com/259389830744794/picture?height=500";
 
 export default class App extends Component {
   render() {
-    return <View style={styles.card} />;
+    return (
+      <View style={styles.card}>
+        <Image style={{ flex: 1 }} source={{ uri: fbImage }} />
+        <View style={{ margin: 20 }}>
+          <Text style={{ fontSize: 20 }}>Candice, 28</Text>
+          <Text style={{ fontSize: 15, color: "darkgrey" }}>Supermodel</Text>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    overflow: "hidden",
     backgroundColor: "white",
     margin: 10,
     marginTop: 100,
